ff87760662992b5cf11d069ffaf2b3fff16ea699
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-04-29 22:03:23 -0300

    feat: add sum and sub functions with corresponding tests
    This commit introduces two new utility functions, `sum` and `sub`, in the server.ts file. Additionally, the sum.test.ts file has been updated to include tests for these functions. A new test script has also been added to the package.json to facilitate running tests with Jest.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,3 +1,9 @@
+import {sum, sub} from "./server";
+
 test("example", ()=>{
-    console.log(" Teste Ok!")
+    const result = sum(1 , 2)
+    const result2 = sub(1, 2)
+    
+    expect(result).toBe(3)
+    expect(result2).toBe(-1)
 })
